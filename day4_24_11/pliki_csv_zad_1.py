a527f60ba25de787b39f5720d41bcb9d994c603e
Kaolek <karolkontek@interia.com>
2024-11-24 14:53:54 +0100

    .

--- day4_24_11/pliki_csv_zad_1.py
+++ day4_24_11/pliki_csv_zad_1.py
@@ -20,3 +20,17 @@ with open("dane/records_3.csv", 'w') as csv_f:
     csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields)
     csv_dict_writer.writeheader()
     csv_dict_writer.writerow(zipped_dict)
+
+products = [
+    {'sku': 1, 'exp_date': 'today', 'price': 100.00},
+    {'sku': 2, 'exp_date': 'today', 'price': 200.00},
+    {'sku': 3, 'exp_date': 'tomorrow', 'price': 250},
+    {'sku': 4, 'exp_date': 'today', 'price': 50},
+    {'sku': 5, 'exp_date': 'today', 'price': 500.00},
+]
+fields_product = [k for k in products[0]]
+
+with open("dane/records_discount.csv", 'w') as csv_f:
+    csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields_product, delimiter=";")
+    csv_dict_writer.writeheader()
+    csv_dict_writer.writerows(products)
