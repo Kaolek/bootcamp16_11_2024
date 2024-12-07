5d79179433eaf171594c1f7a8890a45d9358b9ae
Kaolek <karolkontek@interia.com>
2024-12-07 15:37:56 +0100

    .

---
+++ day_5_7_12/matematyka.py
@@ -0,0 +1,41 @@
+import math
+
+x = min(5, 10, 20)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+print(x)
+print(4 ** 3)
+
+print(math.pi)
+
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+print(x)
+
+y = math.floor(1.4)
+print(y)
+
+x = math.cos(90)
+print(x)
+radian = math.radians(180)
+print(radian)
+x = math.cos(radian)
+print(x)
+
+x = math.exp(2)
+print(f"e ^ 2 = {x}")
+
+log_val = math.log(10)
+print(f"Log: {log_val}")
+
+log10_val = math.log10(10)
+print(f"Log: {log10_val}")
