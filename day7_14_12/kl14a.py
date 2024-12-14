e29aecc9a5ae62bf6923a59600e09fa17e7a8752
Kaolek <karolkontek@interia.com>
2024-12-14 15:11:59 +0100

    .

--- day7_14_12/kl14a.py
+++ day7_14_12/kl14a.py
@@ -10,6 +10,27 @@ class Matematyka:
 
 
 wynik = Matematyka.dodaj(5, 6)
-print(wynik) # 11
+print(wynik)  # 11
 wynik = Matematyka.odejmij(65, 34)
-print(wynik) # 31
+print(wynik)  # 31
+
+
+# klasa metodami statycznymi
+# celcjusz na frenheit
+# farenheit na celcjusz
+
+class KalkulatorTemperatur:
+
+    @staticmethod
+    def celcius_to_farenheit(celcius):
+        return celcius * 9 / 5 + 32
+
+    @staticmethod
+    def farenheit_to_celcius(farenheit):
+        return (farenheit - 32) * 5 / 9
+
+
+print(KalkulatorTemperatur.farenheit_to_celcius(100))  # 37.77777777777778
+print(KalkulatorTemperatur.celcius_to_farenheit(2))  # 35.6
+print(KalkulatorTemperatur.celcius_to_farenheit(37.7777777777777778))
+assert 100.0 == KalkulatorTemperatur.celcius_to_farenheit(37.7777777777777778)
