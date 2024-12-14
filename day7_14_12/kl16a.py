64f16f5750f60dcbc3848e55631f2744ec842292
Kaolek <karolkontek@interia.com>
2024-12-14 15:45:18 +0100

    .

---
+++ day7_14_12/kl16a.py
@@ -0,0 +1,32 @@
+# rson:
+# init__(self, first_name, last_name, id):
+# lf.first_name = first_name
+# lf.last_name = last_name
+# lf.id = id
+#
+#
+# n("Jan", "Kowalski", 1)
+import pickle
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    id: int
+
+    def greet(self):
+        print(self.first_name)
+
+
+p1 = Person("Jan", "Kowalski", 1)
+print(p1)  # Person(first_name='Jan', last_name='Kowalski', id=1)
+p2 = Person("Maciej", "Nowak", 2)
+print(p2)  # Person(first_name='Maciej', last_name='Nowak', id=2)
+
+people = [p1, p2]
+print(people)
+
+with open("dane.pickle", "wb") as stream:
+    pickle.dump(people, stream)
