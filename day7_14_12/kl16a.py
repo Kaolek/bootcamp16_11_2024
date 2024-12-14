12b12ca1a17330b217c5790654dde1e6b720851d
Kaolek <karolkontek@interia.com>
2024-12-14 15:56:15 +0100

    .

--- day7_14_12/kl16a.py
+++ day7_14_12/kl16a.py
@@ -19,14 +19,14 @@ class Person:
     def greet(self):
         print(self.first_name)
 
+if __name__ == '__main__':
+    p1 = Person("Jan", "Kowalski", 1)
+    print(p1)  # Person(first_name='Jan', last_name='Kowalski', id=1)
+    p2 = Person("Maciej", "Nowak", 2)
+    print(p2)  # Person(first_name='Maciej', last_name='Nowak', id=2)
 
-p1 = Person("Jan", "Kowalski", 1)
-print(p1)  # Person(first_name='Jan', last_name='Kowalski', id=1)
-p2 = Person("Maciej", "Nowak", 2)
-print(p2)  # Person(first_name='Maciej', last_name='Nowak', id=2)
-
-people = [p1, p2]
-print(people)
+    people = [p1, p2]
+    print(people)
 
 with open("dane.pickle", "wb") as stream:
     pickle.dump(people, stream)
