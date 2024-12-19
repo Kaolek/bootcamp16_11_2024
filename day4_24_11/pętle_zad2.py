59e409b908dcbcff87f1c13d89316b574f8cc1d7
Kaolek <karolkontek@interia.com>
2024-12-19 21:10:47 +0100

    .

--- day4_24_11/pętle_zad2.py
+++ day4_24_11/pętle_zad2.py
@@ -1,6 +1,7 @@
 dictionary = {'imię': "Radek", 'nazwisko': "Kowalski"}
 print(dictionary)  # {'imię': 'Radek', 'nazwisko': 'Kowalski'}
 print(type(dictionary))  # <class 'dict'>
+
 # wypisanie kluczy
 for k in dictionary:
     print(k)
@@ -9,16 +10,27 @@ for k in dictionary:
 
 for k in dictionary.keys():
     print(k)
+# imię
+# nazwisko
 
+# wypisanie wartości
 for v in dictionary.values():
     print(v)
+# Radek
+# Kowalski
 
+# wypisanie par
 for i in dictionary.items():
     print(i)
+# ('imie', 'Radek')
+# ('nazwisko', 'Kowalski')
 
 for k, v in dictionary.items():
     print(k, v, sep=" <=> ")
+# imie <=> Radek
+# nazwisko <=> Kowalski
 
 for k, v in dictionary.items():
     print(k, ":", v)
-
+# imie : Radek
+# nazwisko : Kowalski
