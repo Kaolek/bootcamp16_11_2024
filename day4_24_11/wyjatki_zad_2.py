7038647e9aef874dac3186d38bd31ebe6aa9bfcc
Kaolek <karolkontek@interia.com>
2024-12-19 21:34:16 +0100

    .

--- day4_24_11/wyjatki_zad_2.py
+++ day4_24_11/wyjatki_zad_2.py
@@ -1,40 +1,41 @@
-def kalkulator():
-    print("Witaj w prostym kalkulatorze!")
-    print("Wpisz 'wyjscie' aby zakończyć program.")
+# napisac program kalkulator z wykorzystaniem głownej petli while True
+# przechwycic możliwe wyjątki i obsłużyc
+# ładnie wypisac wynik np.: Dodawanie 2 + 4 = 6 - fstring
+#
+# wyswietlic opcje do wyboru (dodac opcje zakończenia)
+# pobrac opcje od uzytkownika
+# w zaleznosci od wybranej opcji wykonac działanie i wyswietlic wynik
 
-    while True:
-        operacja = input("Podaj operację (+, -, *, /): ")
-
-        if operacja == 'wyjscie':
-            print("Kończenie programu. Do widzenia!")
-            break
-
-        if operacja in ['+', '-', '*', '/']:
-            try:
-                liczba1 = float(input("Podaj pierwszą liczbę: "))
-                liczba2 = float(input("Podaj drugą liczbę: "))
-
-                if operacja == '+':
-                    wynik = liczba1 + liczba2
-                elif operacja == '-':
-                    wynik = liczba1 - liczba2
-                elif operacja == '*':
-                    wynik = liczba1 * liczba2
-                elif operacja == '/':
-                    if liczba2 != 0:
-                        wynik = liczba1 / liczba2
-                    else:
-                        print("Błąd: Dzielenie przez 0!")
-                        continue
-
-                print(f"Wynik: {wynik}")
-
-            except ValueError:
-                print("Błąd: Niepoprawna wartość! Podaj liczby.")
+while True:
+    print("""
+    1. Dodawanie
+    2. Odejmowanie
+    3. Mnożenie
+    4. Dzielenie
+    5. Koniec
+    """)
+    odp = input("Podaj dopcje menu")
+    # if odp == "5":
+    #     break
+    if odp not in ["1", "2", "3", "4"]:
+        break
+    try:
+        a = int(input("Podaj pierwszą liczby"))
+        b = float(input("Podaj drugą liczbę"))
+        if odp == "1":
+            print(f"Wynik dodawania {a} + {b} = {a + b}")
+        elif odp == "2":
+            print(f"Wynik odejmowania {a} - {b} = {a - b}")
+        elif odp == "3":
+            print(f"Wynik mnożenia {a} * {b} = {a * b}")
+        elif odp == "4":
+            print(f"Wynik dzielenia {a} / {b} = {a / b}")
         else:
-            print("Błąd: Niepoprawna operacja! Spróbuj ponownie.")
-
-
-if __name__ == "__main__":
-    kalkulator()
+            print("Nie ma takiego działania")
+    except ZeroDivisionError:
+        print("Nie dziel przez zero")
+    except Exception as e:
+        print("Błąd", e)
+    else:
+        print("Działanie wykonane poprawnie")
 
