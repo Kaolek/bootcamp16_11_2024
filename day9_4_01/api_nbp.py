53cad7a621e23ec4180d45398d86fd51554fd04a
Kaolek <karolkontek@interia.com>
2025-01-04 11:32:03 +0100

    .

--- day9_4_01/api_nbp.py
+++ day9_4_01/api_nbp.py
@@ -1,6 +1,9 @@
 import requests
-
+from pydantic import BaseModel
+from typing import List
+from datetime import date
 url = "https://api.nbp.pl/api/exchangerates/rates/A/USD/"
+url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
 response = requests.get(url)
 print(response)
 print(response.text)
@@ -11,3 +14,28 @@ print(type(table))
 print(f"Waluta: {table['currency']}")
 print(f"Rates: {table['rates']}")
 print(f"Kurs {table['currency']} wynosi {table['rates'][0]['mid']} pln")
+
+
+# zbudować model klas
+# deserializacja na obiekty
+
+class Rate(BaseModel):
+    no: str
+    effectiveDate: str
+    mid: float
+
+
+class CurrencyData(BaseModel):
+    table: str
+    currency: str
+    code: str
+    rates: List[Rate]
+
+
+currency_data = CurrencyData(**table)
+print(currency_data)
+
+print(currency_data.currency)
+print(currency_data.code)
+print(currency_data.rates[0].mid)
+print(currency_data.rates[0].effectiveDate)
