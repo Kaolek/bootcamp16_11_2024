d8e1f57fba193efa09f36122b6bd58dd78d55871
Kaolek <karolkontek@interia.com>
2025-01-21 21:21:02 +0100

    .

---
+++ day12_12_01/decimal_zad2.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+
+
+def calculate_gross_price(net_price, vat):
+    net_price = Decimal(net_price)
+    vat_rate = Decimal(vat) / 100
+    gross_price = net_price + (net_price * vat_rate)
+    return gross_price.quantize(Decimal("0.01"))
+
+
+price_netto = "100.99"
+vat = 23
+price_brutto = calculate_gross_price(price_netto, vat)
+
+print(f"""
+cena netto: {price_netto}
+Vat: {vat}
+cena brutto: {price_brutto}
+""")
